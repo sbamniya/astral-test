86ddc07a632135a19adc47c96932a61575b020ba
Sonu <sbamniya23@gmail.com>
2025-04-06 18:06:35 +0530

    fix: update config to support chrome-aws-lambda

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      // Exclude .js.map files from server build
+      config.module.rules.push({
+        test: /\.js\.map$/,
+        use: 'ignore-loader',
+      });
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
